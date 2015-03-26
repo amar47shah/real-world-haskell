0fcf5d1a66bfc3d20c3d7179d846220ce5a1271d
Amar Shah <amar47shah@gmail.com>
2015-03-26 10:21:41 -0400

    Complete Chapter 4, exercise 3

---
+++ ch04/FirstWords.hs
@@ -0,0 +1,33 @@
+import System.Environment (getArgs)
+
+interactWith function inputFile outputFile = do
+  input <- readFile inputFile
+  writeFile outputFile (function input)
+
+main = mainWith myFunction
+  where mainWith function = do
+          args <- getArgs
+          case args of
+            [input, output] -> interactWith function input output
+            _ -> putStrLn "error: exactly two arguments needed"
+        myFunction = firstWords
+
+firstWords :: String -> String
+firstWords input = unlines . map firstWord $ splitLines input
+
+firstWord :: String -> String
+firstWord "" = ""
+firstWord s  = head $ words s
+
+splitLines :: String -> [String]
+splitLines [] = []
+splitLines cs =
+  let (pre, suf) = break isLineTerminator cs
+  in pre : case suf of
+                ('\r':'\n':rest) -> splitLines rest
+                ('\r':rest)      -> splitLines rest
+                ('\n':rest)      -> splitLines rest
+                _                -> []
+
+isLineTerminator :: Char -> Bool
+isLineTerminator c = c == '\r' || c == '\n'
