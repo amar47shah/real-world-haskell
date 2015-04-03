df5eafd4b2c2cc356a687745c5da9a2f0cb70334
Amar Shah <amar47shah@gmail.com>
2015-04-02 23:13:18 -0400

    Complete Chapter 4, exercise 4
    'Write a program that transposes the text in a file. For instance,
     it should convert "hello\nworld\n" to "hw\neo\nlr\nll\nod\n".'
    
    As implemented here, only the first two lines are used; all others
    are ignored. Input files that don't contain more than one line cause
    exceptions from Prelude.head.

---
+++ ch04/TransposeTwoLines.hs
@@ -0,0 +1,37 @@
+import System.Environment (getArgs)
+
+interactWith function inputFile outputFile = do
+  input <- readFile inputFile
+  writeFile outputFile (function input)
+
+main = mainWith myFunction
+  where mainWith function = do
+          args <- getArgs
+          case args of
+            [input, output] -> interactWith function input output
+            _ -> putStrLn "error: exactly two arguments needed"
+        myFunction = transposeTwoLines
+
+transposeTwoLines :: String -> String
+transposeTwoLines input = unlines . map (\(x, y) -> [x, y]) . uncurriedZip $ firstTwoLines input
+
+uncurriedZip :: ([a], [b]) -> [(a, b)]
+uncurriedZip = uncurry zip
+
+firstTwoLines :: String -> (String, String)
+firstTwoLines input =
+  let lines = splitLines input
+  in (head $ lines, head . tail $ lines)
+
+splitLines :: String -> [String]
+splitLines [] = []
+splitLines cs =
+  let (pre, suf) = break isLineTerminator cs
+  in pre : case suf of
+                ('\r':'\n':rest) -> splitLines rest
+                ('\r':rest)      -> splitLines rest
+                ('\n':rest)      -> splitLines rest
+                _                -> []
+
+isLineTerminator :: Char -> Bool
+isLineTerminator c = c == '\r' || c == '\n'
