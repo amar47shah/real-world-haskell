f1dcd1f31476e47e7b54fee644377c6dc12f58f7
Amar Shah <amar@shareprogress.org>
2015-04-30 02:29:47 -0400

    Complete exercises 4.2.1 through 4.2.3

--- ch04/ch04.exercises.hs
+++ ch04/ch04.exercises.hs
@@ -1,3 +1,6 @@
+import Data.Char (digitToInt)
+import Data.List (isInfixOf)
+
 -- Chapter 4, Section 1, Exercise 1
 safeHead :: [a] -> Maybe a
 safeHead []    = Nothing
@@ -25,3 +28,30 @@ splitWith f xs = case dropWhile f xs of
                       [] -> []
                       ys -> z : splitWith f zs
                             where (z, zs) = break f ys
+
+-- Chapter 4, Section 2, Exercise 1
+asInt_fold :: String -> Int
+asInt_fold cs
+  | "." `isInfixOf` cs = error "Decimals not allowed"
+  |     18 < length cs = error "Number too large"
+asInt_fold ""          = error "No digits in string"
+asInt_fold ('-':cs)    = -1 * asInt_fold cs
+asInt_fold      cs     = fst $ foldr step (0, 1) cs
+  where step c (sum, power) = (sum + power * digitToInt c, power * 10)
+
+-- Chapter 4, Section 2, Exercise 2
+type ErrorMessage = String
+asInt_either :: String -> Either ErrorMessage Int
+asInt_either cs
+  | "." `isInfixOf` cs  = Left "Decimals not allowed"
+  |     18 < length cs  = Left "Number too large"
+asInt_either    ""      = Left "No digits in string"
+asInt_either   ('-':cs) = case asInt_either cs of
+                            Right int -> Right (-1 * int)
+                            Left  err -> Left  err
+asInt_either        cs  = Right (fst $ foldr step (0, 1) cs)
+  where step c (sum, power) = (sum + power * digitToInt c, power * 10)
+
+-- Chapter 4, Section 2, Exercise 3
+myConcat :: [[a]] -> [a]
+myConcat = foldr (++) []
