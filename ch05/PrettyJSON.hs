99a4a3ecdb0e81b43e36c6101cea17161d74d5a8
Amar Shah <amar@shareprogress.org>
2015-05-08 14:35:15 -0400

    Encapsulate helper

--- ch05/PrettyJSON.hs
+++ ch05/PrettyJSON.hs
@@ -18,12 +18,10 @@ renderJValue (JArray ary)  = series '[' ']' renderJValue ary
 renderJValue (JObject obj) = series '{' '}' field obj
   where field (name, val)  = string name <> text ": " <> renderJValue val
 
-enclose :: Char -> Char -> Doc -> Doc
-enclose left right x = char left <> x <> char right
-
 series :: Char -> Char -> (a -> Doc) -> [a] -> Doc
-series open close item = enclose open close
-                       . fsep . punctuate (char ',') . map item
+series open close item =
+  enclose open close . fsep . punctuate (char ',') . map item
+  where enclose left right x = char left <> x <> char right
 
 string :: String -> Doc
 string = text . concatMap oneChar
