0f4e8359739e2724e5247ee1f748e2132c387c77
Amar Shah <amar@shareprogress.org>
2015-05-08 14:20:28 -0400

    Correct string splitting bug
    The PrettyJSON.string function (String -> Doc) did not return a value like
    
    Text "abc"
    
    but something more like
    
    Concat (Char 'a') (Concat (Char 'b') (Char 'c'))
    
    This unnecessarily complicated the PrettyJSON data model.

--- ch05/PrettyJSON.hs
+++ ch05/PrettyJSON.hs
@@ -26,32 +26,32 @@ series open close item = enclose open close
                        . fsep . punctuate (char ',') . map item
 
 string :: String -> Doc
-string = enclose '"' '"' . hcat . map oneChar
-  where oneChar :: Char -> Doc
+string = text . concatMap oneChar
+  where oneChar :: Char -> String
         oneChar c = case lookup c simpleEscapes of
-                      Just r -> text r
+                      Just r -> r
                       Nothing
                         | mustEscape c -> hexEscape c
-                        | otherwise    -> char c
+                        | otherwise    -> c:""
             where mustEscape c = c < ' ' || c == '\x7f' || c > '\xff'
 
                   simpleEscapes :: [(Char, String)]
                   simpleEscapes = zipWith ch "\b\n\f\r\t\\\"/" "bnfrt\\\"/"
                     where ch a b = (a, ['\\',b])
 
-                  hexEscape :: Char -> Doc
+                  hexEscape :: Char -> String
                   hexEscape c | d < 0x10000 = smallHex d
                               | otherwise   = astral (d - 0x10000)
                     where d = ord c
 
-                          astral :: Int -> Doc
+                          astral :: Int -> String
                           astral n = smallHex (a + 0xd800)
-                                  <> smallHex (b + 0xdc00)
+                                  ++ smallHex (b + 0xdc00)
                             where a = (n `shiftR` 10) .&. 0x3ff
                                   b = n .&. 0x3ff
 
-                          smallHex :: Int -> Doc
-                          smallHex x = text "\\u"
-                                    <> text (replicate (4 - length h) '0')
-                                    <> text h
+                          smallHex :: Int -> String
+                          smallHex x = "\\u"
+                                    ++ (replicate (4 - length h) '0')
+                                    ++ h
                             where h = showHex x ""
